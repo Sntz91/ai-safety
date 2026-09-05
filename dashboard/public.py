2ae134805e0fc7f643cff2f156c3a16fa20ec45e
Tobias Ziegler <tobias.ziegler.8@gmail.com>
2026-09-05 08:41:49 +0200

    no model pages

--- dashboard/public.py
+++ dashboard/public.py
@@ -11,7 +11,6 @@ base_dir = Path(__file__).parent
 pages = {
     "Overview": [
         st.Page(str(base_dir / "pages/02_architecture.py"), title="Architecture"),
-        st.Page(str(base_dir / "pages/06_failure_analysis.py"), title="Failure Analysis"),
     ],
     "Data": [
         st.Page(str(base_dir / "pages/01_data_registry.py"), title="Data Registry")
